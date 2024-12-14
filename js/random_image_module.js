0c3712823649ff79c04a4e0f90f3e0291103046e
kincaidjake <kincaidjake@gmail.com>
2024-12-13 19:00:22 -0800

    Update random_image_module.js

--- js/random_image_module.js
+++ js/random_image_module.js
@@ -1,8 +1,17 @@
 function getRandomImage() {
     const images = [
-        'img/image-01.png',
-        'img/image-02.png',
-        'img/image-03.png'
+        {
+            src: 'img/image-01.png',
+            srcset: 'img/image-01@2x.png 2x',
+        },
+        {
+            src: 'img/image-02.png',
+            srcset: 'img/image-02@2x.png 2x',
+        },
+        {
+            src: 'img/image-03.png',
+            srcset: 'img/image-03@2x.png 2x',
+        }
     ];
 
     const randomIndex = Math.floor(Math.random() * images.length);
@@ -12,8 +21,14 @@ function getRandomImage() {
 function displayRandomImage() {
     const imageContainer = document.getElementById('random-image-container');
     const imgElement = document.createElement('img');
-    imgElement.src = getRandomImage();
+    const randomImage = getRandomImage();
+
+    imgElement.src = randomImage.src;
+    imgElement.srcset = randomImage.srcset;
     imgElement.alt = 'Random Image';
+
+    // Clear the container before adding a new image (optional)
+    imageContainer.innerHTML = '';
     imageContainer.appendChild(imgElement);
 }
 
